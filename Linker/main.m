e36d60742102923fda1c6d5e2032720d17ea090c
Zvigun Alexandr <mircamnia@ukr.net>
2016-09-14 22:50:19 +0300

    Initial Commit

---
+++ Linker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Linker
+//
+//  Created by Mac on 14.09.16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
